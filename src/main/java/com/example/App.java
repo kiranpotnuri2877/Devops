686c014149757da705caa7d5a41108f3eaa4944a
kiranpotnuri2877 <kiranpotnuri287@gmail.com>
2026-07-14 12:06:07 +0530

    web server added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello DevOps from Jenkins");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello, this is my Jenkins CI/CD deployed app!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 8081");
     }
 }
